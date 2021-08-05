fe42e812f1465d2b11f6b4887e243f5bf5c8608e
razvanr91 <razvanroman1991@gmail.com>
2021-08-05 13:32:33 +0100

    Create Comment Model

---
+++ Models/Comment.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RomanWrites.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public int PostId { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string ModeratorId { get; set; }
+
+        [Required]
+        [StringLength(250, ErrorMessage = "The {0} must be between {2} and {1} characters long", MinimumLength = 2)]
+        [Display(Name = "Comment")]
+        public string Body { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime Created { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? Updated { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? Moderated { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? Deleted { get; set; }
+
+        [StringLength(250, ErrorMessage = "The {0} must be between {2} and {1} characters long", MinimumLength = 2)]
+        [Display(Name = "Moderated")]
+        public string ModeratedBody { get; set; }
+
+        public virtual Post Post { get; set; }
+
+        public virtual IdentityUser Author { get; set; }
+
+    }
+}
