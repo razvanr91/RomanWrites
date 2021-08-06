ec5249e8e99c49858f5610647d957201d07533f3
razvanr91 <razvanroman1991@gmail.com>
2021-08-06 12:06:12 +0100

    Update Blog Model
    Add Navigation Properties to Blog Model

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -34,5 +35,11 @@ namespace RomanWrites.Models
 
         [NotMapped]
         public IFormFile Image { get; set; }
+
+        //Navigation Property
+        public virtual IdentityUser Author { get; set; }
+
+        public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
+
     }
 }
