34e4f828fee7b1a7d25eddf98c749ecdcf913939
razvanr91 <razvanroman1991@gmail.com>
2021-08-06 12:00:19 +0100

    Create Tag Model

---
+++ Models/Tag.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace RomanWrites.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+
+        public int PostId { get; set; }
+
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be between {2} and {1} characters long", MinimumLength = 2)]
+        public string Text { get; set; }
+
+        public virtual Post Post { get; set; }
+
+        public virtual Blog Blog { get; set; }
+
+        public virtual IdentityUser Author { get; set; }
+    }
+}
