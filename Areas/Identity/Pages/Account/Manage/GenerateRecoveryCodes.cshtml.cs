e8a4c79a4a3a3e0ba77b969cb68cd270297f4b07
razvanr91 <razvanroman1991@gmail.com>
2021-08-09 00:52:10 +0100

    Update GenerateRecoveryCodes.cshtml.cs

--- Areas/Identity/Pages/Account/Manage/GenerateRecoveryCodes.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/GenerateRecoveryCodes.cshtml.cs
@@ -6,16 +6,17 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using RomanWrites.Models;
 
 namespace RomanWrites.Areas.Identity.Pages.Account.Manage
 {
     public class GenerateRecoveryCodesModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<BlogUser> _userManager;
         private readonly ILogger<GenerateRecoveryCodesModel> _logger;
 
         public GenerateRecoveryCodesModel(
-            UserManager<IdentityUser> userManager,
+            UserManager<BlogUser> userManager,
             ILogger<GenerateRecoveryCodesModel> logger)
         {
             _userManager = userManager;
