98fdfb5ae2fbf74c62f888e8a41fa5a4ea5f5fb6
razvanr91 <razvanroman1991@gmail.com>
2021-08-09 00:48:11 +0100

    Update EnableAuthenticator.cshtml.cs

--- Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -15,14 +15,14 @@ namespace RomanWrites.Areas.Identity.Pages.Account.Manage
 {
     public class EnableAuthenticatorModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<BlogUser> _userManager;
         private readonly ILogger<EnableAuthenticatorModel> _logger;
         private readonly UrlEncoder _urlEncoder;
 
         private const string AuthenticatorUriFormat = "otpauth://totp/{0}:{1}?secret={2}&issuer={0}&digits=6";
 
         public EnableAuthenticatorModel(
-            UserManager<IdentityUser> userManager,
+            UserManager<BlogUser> userManager,
             ILogger<EnableAuthenticatorModel> logger,
             UrlEncoder urlEncoder)
         {
