6f9c59080639ce377cda2486eab2def2a58d9ec0
razvanr91 <razvanroman1991@gmail.com>
2021-08-09 00:43:22 +0100

    Update LoginWith2fa.cshtml.cs

--- Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
+++ Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
@@ -8,16 +8,17 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using RomanWrites.Models;
 
 namespace RomanWrites.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class LoginWith2faModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly SignInManager<BlogUser> _signInManager;
         private readonly ILogger<LoginWith2faModel> _logger;
 
-        public LoginWith2faModel(SignInManager<IdentityUser> signInManager, ILogger<LoginWith2faModel> logger)
+        public LoginWith2faModel(SignInManager<BlogUser> signInManager, ILogger<LoginWith2faModel> logger)
         {
             _signInManager = signInManager;
             _logger = logger;
