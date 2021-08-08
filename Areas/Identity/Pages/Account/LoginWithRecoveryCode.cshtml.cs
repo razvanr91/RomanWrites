4a82489ad8a5ff3587383a7df5b4c2b8f5d40eff
razvanr91 <razvanroman1991@gmail.com>
2021-08-09 00:42:53 +0100

    Update LoginWithRecoveryCode.cshtml.cs

--- Areas/Identity/Pages/Account/LoginWithRecoveryCode.cshtml.cs
+++ Areas/Identity/Pages/Account/LoginWithRecoveryCode.cshtml.cs
@@ -8,16 +8,17 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using RomanWrites.Models;
 
 namespace RomanWrites.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class LoginWithRecoveryCodeModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly SignInManager<BlogUser> _signInManager;
         private readonly ILogger<LoginWithRecoveryCodeModel> _logger;
 
-        public LoginWithRecoveryCodeModel(SignInManager<IdentityUser> signInManager, ILogger<LoginWithRecoveryCodeModel> logger)
+        public LoginWithRecoveryCodeModel(SignInManager<BlogUser> signInManager, ILogger<LoginWithRecoveryCodeModel> logger)
         {
             _signInManager = signInManager;
             _logger = logger;
