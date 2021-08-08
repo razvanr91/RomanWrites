58c49b9a0c477d197914a90cd9a735d3d43a4c02
razvanr91 <razvanroman1991@gmail.com>
2021-08-08 23:55:05 +0100

    Update EmailService.cs

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using RomanWrites.Data;
+using RomanWrites.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +10,14 @@ namespace RomanWrites.Services
 {
     public class EmailService : IBlogEmailSender
     {
+        private readonly ApplicationDbContext _context;
+        private readonly MailSettings _mailSettings;
+
+        public EmailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
         public Task SendContactEmailAsync(string emailFrom, string name, string subject, string message)
         {
             throw new NotImplementedException();
