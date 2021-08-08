d11934ef4c403721d70251e8978f2fc3a2585c0d
razvanr91 <razvanroman1991@gmail.com>
2021-08-08 23:47:08 +0100

    Update IBlogEmailSender.cs

--- Services/IBlogEmailSender.cs
+++ Services/IBlogEmailSender.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RomanWrites.Services
 {
-    public interface IBlogEmailSender
+    public interface IBlogEmailSender : IEmailSender
     {
-
+        Task SendContactEmailAsync(string emailFrom, string name, string subject, string message);
     }
 }
