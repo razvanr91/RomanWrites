d137137a5cf5fd1ea340b8feb9d383e49c3f9c5c
razvanr91 <razvanroman1991@gmail.com>
2021-08-09 00:46:06 +0100

    Update ChangePassword.cshtml.cs

--- Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -7,17 +7,19 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using RomanWrites.Models;
+
 namespace RomanWrites.Areas.Identity.Pages.Account.Manage
 {
     public class ChangePasswordModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<BlogUser> _userManager;
+        private readonly SignInManager<BlogUser> _signInManager;
         private readonly ILogger<ChangePasswordModel> _logger;
 
         public ChangePasswordModel(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager,
+            UserManager<BlogUser> userManager,
+            SignInManager<BlogUser> signInManager,
             ILogger<ChangePasswordModel> logger)
         {
             _userManager = userManager;
