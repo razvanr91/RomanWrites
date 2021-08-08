49363c921b30a3550a4b5366e9c5f60836f13c2c
razvanr91 <razvanroman1991@gmail.com>
2021-08-08 23:50:10 +0100

    Register BlogEmailSender and create EmailService

--- Startup.cs
+++ Startup.cs
@@ -50,6 +50,8 @@ namespace RomanWrites
 
             // Configure Mail Settings class
             services.Configure<MailSettings>(Configuration.GetSection("MailSettings"));
+
+            services.AddScoped<IBlogEmailSender, EmailService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
