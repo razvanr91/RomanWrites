75a7ecad4256f0dd9a67c877c81546438085fe0a
razvanr91 <razvanroman1991@gmail.com>
2021-08-09 00:44:10 +0100

    Update ForgotPassword.cshtml.cs

--- Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -10,16 +10,18 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using RomanWrites.Services;
+using RomanWrites.Models;
 
 namespace RomanWrites.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class ForgotPasswordModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly IEmailSender _emailSender;
+        private readonly UserManager<BlogUser> _userManager;
+        private readonly IBlogEmailSender _emailSender;
 
-        public ForgotPasswordModel(UserManager<IdentityUser> userManager, IEmailSender emailSender)
+        public ForgotPasswordModel(UserManager<BlogUser> userManager, IBlogEmailSender emailSender)
         {
             _userManager = userManager;
             _emailSender = emailSender;
