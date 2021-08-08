8d211cb37ea8b78bc3ad407f614aca5739c78920
razvanr91 <razvanroman1991@gmail.com>
2021-08-09 00:40:17 +0100

    Inject BlogUser in codebehind

--- Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
+++ Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
@@ -10,16 +10,18 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using RomanWrites.Models;
+using RomanWrites.Services;
 
 namespace RomanWrites.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class ResendEmailConfirmationModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly IEmailSender _emailSender;
+        private readonly UserManager<BlogUser> _userManager;
+        private readonly IBlogEmailSender _emailSender;
 
-        public ResendEmailConfirmationModel(UserManager<IdentityUser> userManager, IEmailSender emailSender)
+        public ResendEmailConfirmationModel(UserManager<BlogUser> userManager, IBlogEmailSender emailSender)
         {
             _userManager = userManager;
             _emailSender = emailSender;
