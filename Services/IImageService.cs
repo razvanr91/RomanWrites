1d2849f08b9361e87adf5ba62481d7815f9a917a
razvanr91 <razvanroman1991@gmail.com>
2021-08-09 12:24:23 +0100

    Create and implement Image Service interface and class

---
+++ Services/IImageService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RomanWrites.Services
+{
+    public interface IImageService
+    {
+        Task<byte[]> EncodeImageAsync(IFormFile file);
+
+        Task<byte[]> EncodeImageAsync(string fileName);
+
+        string DecodeImage(byte[] imageData, string contentType);
+
+        string ContentType(IFormFile file);
+
+        int Size(IFormFile file);
+    }
+}
---
+++ Services/ImageService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RomanWrites.Services
+{
+    public class ImageService : IImageService
+    {
+        public string ContentType(IFormFile file)
+        {
+            return file?.ContentType;
+        }
+
+        public string DecodeImage(byte[] imageData, string contentType)
+        {
+            if ( imageData is null || contentType is null )
+                return null;
+
+            return $"data:image/{contentType};base64,{Convert.ToBase64String(imageData)}";
+        }
+
+        public async Task<byte[]> EncodeImageAsync(IFormFile file)
+        {
+            if ( file is null )
+                return null;
+
+            using var ms = new MemoryStream();
+            await file.CopyToAsync(ms);
+            return ms.ToArray();
+        }
+
+        public async Task<byte[]> EncodeImageAsync(string fileName)
+        {
+            var file = $"{Directory.GetCurrentDirectory()}/wwwroot/assets/img/{fileName}";
+
+            return await File.ReadAllBytesAsync(file);
+        }
+
+        public int Size(IFormFile file)
+        {
+            return Convert.ToInt32(file?.Length);
+        }
+    }
+}
