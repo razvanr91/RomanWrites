87497451521003953be196638abf10e325dfa2f2
razvanr91 <razvanroman1991@gmail.com>
2021-08-09 01:06:58 +0100

    Update ContactMe.cs

--- ViewModels/ContactMe.cs
+++ ViewModels/ContactMe.cs
@@ -8,13 +8,17 @@ namespace RomanWrites.ViewModels
 {
     public class ContactMe
     {
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
         public string Subject { get; set; }
 
+        [Required]
         public string Message { get; set; }
 
     }
