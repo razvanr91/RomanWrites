896fca935c572d20cebe403abca9e1cf63f455b0
razvanr91 <razvanroman1991@gmail.com>
2021-08-15 23:02:57 +0100

    Integrate Sweet alerts
    Use sweet alerts with function to check for empty input or duplicate tag, or check if a tag was selected for deletion

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,17 +5,40 @@ let index = tagList.length;
 function AddTag() {
     var tagEntry = document.getElementById("TagEntry");
 
-    let newOption = new Option(tagEntry.value, tagEntry.value);
+    let searchResult = search(tagEntry.value);
 
-    tagList.options[index++] = newOption;
+    if (searchResult != null) {
+        // Trigger sweet alert for empty or duplicate input
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            html: `${searchResult}`
+        })
+
+    } else {
+        // Create new tag
+        let newOption = new Option(tagEntry.value, tagEntry.value);
+
+        tagList.options[index++] = newOption;
+    }
 
     tagEntry.value = "";
 
     return true;
+
 }
 
 function DeleteTag() {
     let tagCount = 1;
+
+    if (tagList.selectedIndex === -1) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            html: `Please select an option to delete`
+        });
+        return true;
+    }
 
     while (tagCount > 0) {
         let selectedIndex = tagList.selectedIndex;
@@ -35,3 +58,19 @@ function DeleteTag() {
 $("form").on("submit", function () {
     $("#TagValues option").prop("selected", "selected");
 })
+
+// Function to detect if the input is empty or the tag is a duplicate
+function search(str) {
+    if (str == "") {
+        return "You have not entered any text";
+    }
+
+    if (tagList) {
+        let options = tagList.options;
+        for (let i = 0; i < options.length; i++) {
+            if (options[i].value === str) {
+                return `<strong>#${str}</strong> already exists in the list.`;
+            }
+        }
+    }
+}
