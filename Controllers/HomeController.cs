18775b76124c31c52f4d233dff474ebb727143cb
razvanr91 <razvanroman1991@gmail.com>
2021-08-16 13:04:21 +0100

    Modify DB query to include the author of blogs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,7 +28,7 @@ namespace RomanWrites.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var blogs = await _context.Blogs.ToListAsync();
+            var blogs = await _context.Blogs.Include(b => b.Author).ToListAsync();
 
             return View(blogs);
         }
