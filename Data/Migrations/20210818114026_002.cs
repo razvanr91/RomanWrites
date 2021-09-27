60a81c2fe9d373df724636e7f6ca21ffdc4dd9ec
razvanr91 <razvanroman1991@gmail.com>
2021-09-27 13:25:49 +0100

    Create App user class

---
+++ Data/Migrations/20210818114026_002.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RomanWrites.Data.Migrations
+{
+    public partial class _002 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
